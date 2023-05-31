d1ecc5b79dad7dfdfa23fbc77c5d804bdfcef57c
Guime2010 <113637031+Guime2010@users.noreply.github.com>
2023-05-31 16:19:35 -0300

    gmae.js

--- gmae.js
+++ gmae.js
@@ -1,7 +1,7 @@
 nome1=localStorage.getItem("nome1")
 nome2=localStorage.getItem("nome2")
-player1score=0
-player2score=0
+player1Score=0
+player2Score=0
 document.getElementById("player1Name").innerHTML=nome1+": "
 document.getElementById("player2Name").innerHTML=nome2+": "
 
@@ -11,35 +11,66 @@ document.getElementById("player2Score").innerHTML=player2Score
 function send(){
     palavra=document.getElementById("word").value
     palavra=palavra.toLowerCase()
+    
     retire1=palavra.charAt(1)
+    
     retire2=Math.floor(palavra.length/2)
-    retire3=palavra.length-1
+    retire2=palavra.charAt(retire2)
+    
+    retire3=palavra.charAt(palavra.length-2)
+
+    
     remover1=palavra.replace(retire1,"_")
     remover2=remover1.replace(retire2,"_")
     remover3=remover2.replace(retire3,"_")
 
-question_word = "<h4 id='wordDisplay'> P. "+remover3+"</h4>";
+    question_word = "<h4 id='wordDisplay'>"+remover3+"</h4>";
     inputBox = "<br>Resposta : <input type='text' id='inputCheckBox'>";
     checkButton = "<br><br><button class='btn btn-info' onclick='check()'>Checar</button>";
     row =  question_word + inputBox + checkButton ; 
     document.getElementById("output").innerHTML = row;
-document.getElementById("word").value = "";
+    document.getElementById("word").value = "";
     
 }
-pergunta="player1";
-resposta="player2"
+turnoPergunta="player1";
+turnoResposta="player2"
 function check(){
-  obterresposta=document.getElementById("inputCheckBox") 
-  obterresposta=obterresposta.toLowerCase()
-  if(palavra==obterresposta){
-    if(resposta=="player1"){
+  obterResposta=document.getElementById("inputCheckBox").value 
+  obterResposta=obterResposta.toLowerCase()
+  if(palavra==obterResposta){
+    if(turnoResposta=="player1"){
         player1Score=player1Score+1
+        console.log(player1Score)
         document.getElementById("player1Score").innerHTML=player1Score
     } 
         else {
-       player2score=player2Score+1
-       document.getElementById("player1Score").innerHTML=player2Score
+       player2Score=player2Score+1
+       document.getElementById("player2Score").innerHTML=player2Score
     }
+
   }
+  if(turnoPergunta=="player1")
+	{
+		turnoPergunta="player2"
+		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta - " + nome2;
+	}
+	else 
+	{
+		turnoPergunta="player1"
+		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta - " + nome1 ;
+	}
+
+	if(turnoResposta=="player1")
+	{
+		turnoResposta="player2"
+		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta - " + nome2 ;
+	}
+	else 
+	{
+		turnoResposta="player1"
+		document.getElementById("playerAnswer").innerHTML = "Turno de Resposta - " + nome1;
+	}
+
+    document.getElementById("output").innerHTML = "";
 }
  
